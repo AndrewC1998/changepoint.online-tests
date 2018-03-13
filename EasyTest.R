6e5afafea085e0832702eeaba4612f8095dd152c
AndrewC1998 <37156349+AndrewC1998@users.noreply.github.com>
2018-03-13 12:36:41 +0000

    Update EasyTest.R

--- EasyTest.R
+++ EasyTest.R
@@ -1,10 +1,12 @@
 n <- length(data)
 for(i in 4:n){
   Sys.sleep(0.1)
-  plot(x=1:i,y=data[1:i],xlim=c(0,n),ylim=range(data))
-  ansvar=cpt.meanvar(data[1:i])
+  plot(x=1:i,y=data[1:i],xlim=c(0,n),ylim=range(data))  #type = "l" can be used for line plot
+  ansvar=cpt.meanvar(data[1:i],method="BinSeg",Q=3,penalty="Manual",pen.value=2*log(n))
   changepoints <- cpts(ansvar)
   if(length(ansvar@cpts)>0){
+    for(i in 1:3){
     abline(v=changepoints, col = "blue")
+    }
   }
-}
+ }
